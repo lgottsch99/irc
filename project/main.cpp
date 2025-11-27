17cf951be3c4d5d9160db6b86d49aa9973d2e8d6
lgottsch99 <lgottsch@student.42prague.com>
2025-11-27 18:42:39 +0100

    nov 27

--- project/main.cpp
+++ project/main.cpp
@@ -2,7 +2,9 @@
 
 /* TODOs
 Server:
+	check empty strings input & num validation
 	polling
+	accepting (enforce pw)
 	signal handling ctrl d, ctrl c
 
 
@@ -15,23 +17,23 @@ int main(int argc, char *argv[])
 {
 	(void)argv;
 	if (argc != 3)
-	{		
+	{
 		std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
 		return 1;
 	}
-
+	
+	Server irc_server;
+	
 	try
-	{
-		Server irc_server;		
+	{	
 		irc_server.startup(argv);
-
 		irc_server.poll_loop(); //can be put into startup() ft later
 
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
-		//irc_server.server_shutdown();
+		irc_server.server_shutdown();
 	}
 	return 0;
 }
